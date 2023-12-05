988324930167ae799ee6873c2d9a864402b88c10
laurin21 <74432244+laurin21@users.noreply.github.com>
2023-12-05 08:52:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,7 @@ st.metric(label = "Heute gelesen", value = f"{int(seiten_heute)}", delta = f"{in
 st.markdown("---")
 
 last_book = int(updates["Buch_ID"].iloc[-1]-1)
+st.table(buecher)
 last_page = str(buecher.loc[buecher['Titel'] == last_book, 'Fortschritt'].values[0])
 st.write(last_page)
 st.markdown("##### Aktuelles Buch")
